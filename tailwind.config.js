4794df646d7068c106bcfa6e73d92eb7f4e8f62e
w3modabbir <modabbirhossen.web@gmail.com>
2023-10-05 23:03:12 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,9 @@ tailwind.config = {
         },
         backgroundImage: {
           'banner': "url('images/banner.jpg')",
+          'visit': "url('images/visit.jpg')",
           'overlay': "linear-gradient(40deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%);",
+          'textcolor': "linear-gradient(0deg, #A54E2B -53.83%, #DC9853 62.61%);",
         }
       }
     }
