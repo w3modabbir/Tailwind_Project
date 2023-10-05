8e68d5f9a6416c30154d18dc8d74dfdb7aa88100
w3modabbir <modabbirhossen.web@gmail.com>
2023-10-05 21:21:37 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'primarycolor': '#1C1D28',
+          'secoundarycolor': '#fff',
+          'peracolor': '#161722',
+          'bottuncolor': '#FFD2A4',
+          'bgcolor': '#FFF5EB',
+          'joinbg': '#F5F2F0'
+        },
+        maxWidth: {
+            'container': '1280px',
+        },
+        fontSize: {
+            'navmamu': '16px',
+        },
+        fontFamily: {
+           'robotoconsider': "Roboto Condensed, sans-serif",
+        },
+        backgroundImage: {
+          'banner': "url('images/banner.jpg')",
+          'overlay': "linear-gradient(40deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%);",
+        }
+      }
+    }
+  }
